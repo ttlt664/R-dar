98fa9f50885ee2178e3cdba6990ba6d96d71ff95
23 <syldyx2020@163.com>
2023-12-02 23:13:58 +0800

    add gin

--- radar_src/main.go
+++ radar_src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-contrib/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/radar", logger.SetLogger(
+		logger.WithSkipPath([]string{"/skip"}),
+	), func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hi hacker",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
