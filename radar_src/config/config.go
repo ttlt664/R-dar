e276ff090c8a6d0fd117d1a7877988693af048ea
ttlt <syldyx2020@163.com>
2023-12-03 23:51:47 +0800

    add files

--- radar_src/config/config.go
+++ radar_src/config/config.go
@@ -1,3 +1,35 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
 const HttpPort = ":7777"
+
+type Config struct {
+	Log struct {
+		Level     string
+		Output    string
+		Formatter string
+		LogPath   string
+	}
+}
+
+// LoadConfig loads the configuration from config.toml file
+func LoadConfig() (*Config, error) {
+	var config Config
+
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
